4ce9074ae8da29bc67906d5ec2427f9017b06987
Yerosh <adilbekernur@gmail.com>
2025-08-21 22:47:51 +0500

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Yernur Adilbek on 21.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
