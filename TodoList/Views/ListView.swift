f607f57f7d2d39887513c303094a48ff251367d5
Yerosh <adilbekernur@gmail.com>
2025-08-21 23:19:50 +0500

    first commit

---
+++ TodoList/Views/ListView.swift
@@ -0,0 +1,38 @@
+//
+//  ListView.swift
+//  TodoList
+//
+//  Created by Yernur Adilbek on 21.08.2025.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    
+    @State var items: [String] = [
+        "This is the first item",
+        "This is the second item",
+        "This is the third item"
+    ]
+    
+    var body: some View {
+        List {
+            ForEach(items, id: \.self) { item in
+                ListRowView(title: item)
+            }
+        }
+        .listStyle(PlainListStyle())
+        .navigationTitle("Todo List 📝")
+        .navigationBarItems(
+            leading: EditButton(),
+            trailing:
+                NavigationLink("Add", destination: AddView())
+        )
+    }
+}
+
+#Preview {
+    NavigationView {
+        ListView()
+    }
+}
