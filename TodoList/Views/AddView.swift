e8c371bfc43fe1a279b9414bf40933fa51586138
Yerosh <adilbekernur@gmail.com>
2025-08-24 17:01:45 +0500

    Added CRUD functions to the ViewModel Part 2

--- TodoList/Views/AddView.swift
+++ TodoList/Views/AddView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct AddView: View {
     
+    @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject var listViewModel: ListViewModel
     @State var textFieldText: String = ""
+    
+    @State var alertTitle: String = ""
+    @State var showAlert: Bool = false
     
     var body: some View {
         ScrollView {
@@ -20,9 +25,7 @@ struct AddView: View {
                     .background(Color(.systemGray5))
                     .cornerRadius(10)
                 
-                Button(action: {
-                    
-                }, label: {
+                Button(action: saveButtonPressed, label: {
                     Text("Save".uppercased())
                         .foregroundColor(.white)
                         .font(.headline)
@@ -37,6 +40,28 @@ struct AddView: View {
 
         }
         .navigationTitle("Add an Item ✏️")
+        .alert(isPresented: $showAlert, content: getAlert) 
+    }
+    
+    func saveButtonPressed() {
+        if textIsAppropriate() {
+            listViewModel.addItem(title: textFieldText)
+            presentationMode.wrappedValue.dismiss()
+        }
+        
+    }
+    
+    func textIsAppropriate() -> Bool {
+        if textFieldText.count < 3 {
+            alertTitle = "Your new todo item must be at least 3 characters long😕"
+            showAlert.toggle()
+            return false
+        }
+        return true
+    }
+    
+    func getAlert() -> Alert {
+        return Alert(title: Text(alertTitle))
     }
 }
 
@@ -44,5 +69,5 @@ struct AddView: View {
     NavigationView {
         AddView()
     }
-    
+    .environmentObject(ListViewModel())
 }
